349efd5f3fe54030e4892dcde4a89521828f8a13
Pallavi-pooja <pallavichindam@gmail.com>
2020-09-03 22:41:44 +0530

    fcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {HeaderComponent} from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './receipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './receipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/receipes/receipes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-receipes',
+  templateUrl: './receipes.component.html',
+  styleUrls: ['./receipes.component.css']
+})
+export class ReceipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
